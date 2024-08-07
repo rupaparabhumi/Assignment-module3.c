689614090fbc988d9b97e64ad0053289abcb4548
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-08-07 15:27:17 +0530

    Add files via upload

---
+++ Answer1-16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+
+char fname[20];
+ printf("Enter The Country's' name:");
+  scanf("%s",&fname);
+
+  printf("Abbreviated Name: ");
+  printf("%c.",fname[0]);
+  return 0;
+
+}
